fe46215109b942181546ca0d983fada0f7558b88
Amit <amitverma3817@gmail.com>
2023-01-17 13:08:27 +0530

    Authentication done

---
+++ app.js
@@ -0,0 +1,27 @@
+const express=require('express');
+const { default: mongoose } = require('mongoose');
+const authroutes=require("./routes/authroutes");
+const cors=require("cors");
+
+
+const app=express();
+require('dotenv').config()
+app.use(cors({origin:true}));
+app.use(express.json());
+
+
+mongoose.connect(process.env.dburl)
+  .then(result=>{
+    app.listen(2000);
+    console.log("connected");
+}).catch(err=>{
+    console.log(err);
+});
+app.use(authroutes);
+app.use((error, req, res, next) => {
+  console.log(error);
+  const status = error.statusCode || 500;
+  const message = error.message;
+  const data = error.data;
+  res.status(status).json({ message: message, data: data });
+});
